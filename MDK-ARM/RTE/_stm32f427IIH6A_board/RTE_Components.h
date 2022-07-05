b1ef4a73ce860898fb752da1d516e63252d97888
nichijou <1534150771@qq.com>
2022-07-05 21:37:48 +0800

    all file init

---
+++ MDK-ARM/RTE/_stm32f427IIH6A_board/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'stm32f427IIH6A_board' 
+ * Target:  'stm32f427IIH6A_board' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
